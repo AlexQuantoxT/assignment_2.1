d0054640cf5bd9f80db202efbf2008d74f8b689e
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-04 13:28:57 +0200

    Added a skeleton for all crud_classes

---
+++ src/classes/crud_classes/GroupCRUD.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MyApp\classes\crud_classes;
+use \MyApp\classes\ConnDB;
+use MyApp\interfaces\CRUD;
+
+class GroupCRUD implements CRUD{
+    private $conn;
+    public function __construct(ConnDB $conn)
+    {
+        $this->conn = $conn->connect();
+    }
+    public function create()
+    {
+        
+    }
+    public function read()
+    {
+        
+    }
+    public function update()
+    {
+        
+    }
+    public function delete()
+    {
+        
+    }
+}
