c131912499c6d6818aa39f8e767592afa80e184a
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-04 15:19:22 +0200

    Added crud functions to Comments

--- src/classes/crud_classes/CommentCRUD.php
+++ src/classes/crud_classes/CommentCRUD.php
@@ -12,18 +12,33 @@ class CommentCRUD implements CRUD{
     }
     public function create()
     {
-        
+        $sql = "INSERT INTO comments VALUES(null,:comment,:mentor_id,intern_id,CURRENT_TIMESTAMP)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function read()
     {
-        
+        $sql = "SELECT comments.comment_id,comments.comment_text,comments.mentor_id,users.name 
+                AS intern_name,users.lastname as intern_lastname,comments.intern_id 
+                FROM (comments LEFT JOIN users ON intern_id = users.user_id)";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function update()
     {
-        
+        $sql = "UPDATE comments 
+                SET comments.comment_text = :commetn_text,comments.comment_time = CURRENT_TIMESTAMP 
+                WHERE comments.comment_id = comment_id";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function delete()
     {
-        
+        $sql = "DELETE FROM comments WHERE comments.comment_id = :comment_id";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
 }
+// //test
+// $test = new UserCRUD(new ConnDB);
+// print_r($test->create());
