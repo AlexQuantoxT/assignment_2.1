9d4acf2fae29017af8ab72e8872cb1934494f810
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-04 14:41:38 +0200

    Added crud functions to Users

--- src/classes/crud_classes/UserCRUD.php
+++ src/classes/crud_classes/UserCRUD.php
@@ -3,7 +3,7 @@
 namespace MyApp\classes\crud_classes;
 use \MyApp\classes\ConnDB;
 use MyApp\interfaces\CRUD;
-
+require_once '../../../vendor/autoload.php';
 class UserCRUD implements CRUD{
     private $conn;
     public function __construct(ConnDB $conn)
@@ -12,18 +12,33 @@ class UserCRUD implements CRUD{
     }
     public function create()
     {
-        
+        $sql = "INSERT INTO users VALUES(null,:user_name,:user_lastname,:role_id,:group_id);";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function read()
     {
-        
+        $sql = "SELECT users.user_id,users.name,users.lastname,groups.group_id, groups.group_name,roles.role_name
+                FROM ((users LEFT JOIN groups ON users.user_id = groups.group_id)
+                LEFT JOIN roles ON users.role_id = roles.role_id) WHERE users.user_id = :user_id ORDER BY users.name ASC;";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function update()
     {
-        
+        $sql = "UPDATE users SET users.name = :user_name, users.lastname = :user_lastname, users.role_id = :role_id, users.group_id = :group_id 
+                WHERE users.user_id = :user_id;";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
     public function delete()
     {
-        
+        $sql = "DELETE FROM users WHERE users.user_id = :user_id";
+        $stmt = $this->conn->prepare($sql);
+        return $stmt;
     }
 }
+
+// //test
+// $test = new UserCRUD(new ConnDB);
+// print_r($test->create());
