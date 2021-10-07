e0f158350be0add9435beeceeb54e0b249221223
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-07 19:54:18 +0200

    adding comments

--- src/interfaces/CRUD.php
+++ src/interfaces/CRUD.php
@@ -1,10 +1,41 @@
 <?php
 namespace MyApp\interfaces;
 
+/** 
+*
+* Interface for CRUD functions
+*
+*/
+
 interface CRUD{
+    /** 
+    *
+    * For preparing create sql query
+    *
+    */
     public function create();
+    /** 
+    *
+    * For preparing read all sql query
+    *
+    */
     public function readAll();
+    /** 
+    *
+    * For preparing read sql query
+    *
+    */
     public function read();
+    /** 
+    *
+    * For preparing update sql query
+    *
+    */
     public function update();
+    /** 
+    *
+    * For preparing delete sql query
+    *
+    */
     public function delete();
 }
--- src/interfaces/Request.php
+++ src/interfaces/Request.php
@@ -1,9 +1,35 @@
 <?php
 namespace MyApp\interfaces;
 
+/** 
+*
+* Interface for request functions
+*
+*/
+
 interface Request{
+    /** 
+    *
+    * For processing post request
+    *
+    */
     public function post();
+    /** 
+    *
+    * For processing get request
+    *
+    */
     public function get();
+    /** 
+    *
+    * For processing patch request
+    *
+    */
     public function patch();
+    /** 
+    *
+    * For processing delete request
+    *
+    */
     public function delete();
 }
