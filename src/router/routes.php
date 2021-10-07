ec0661bb356d4ae74077fc41cfe95abd11ea9e79
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-07 21:20:36 +0200

    testing router

--- src/router/routes.php
+++ src/router/routes.php
@@ -1,9 +1,8 @@
 <?php
+// echo __DIR__ . "/assignment_2.1/src/router/router.php";
+require_once(__DIR__ . "/router.php");
 
-require_once(__DIR__.'/router.php');
+get('/mentor', '../apis/MentorApi.php');
 
 
-// get('/mentor', 'MentorApi.php');
-
-
-any('/404','src/router/404.php');
+any('/404', 'www/assignment_2.1/src/router/404.php');
