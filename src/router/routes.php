52ba2199ad6c0c43f3a38f7ad78599f60aaaff96
AlexQuantoxT <aleksa.kevresan@quantoxtechnology.com>
2021-10-07 20:36:24 +0200

    testing router

---
+++ src/router/routes.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(__DIR__.'/router.php');
+
+
+// get('/mentor', 'MentorApi.php');
+
+
+any('/404','src/router/404.php');
---
+++ views/404.php
@@ -0,0 +1,3 @@
+<?php
+echo "ERROR 404, PAGE NOT FOUND.";
+http_response_code(404);
